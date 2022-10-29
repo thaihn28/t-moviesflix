b2ca4883a1d3c846125efb40e12e9e3dde672378
thaihn28 <hoangthai.solem2801@gmail.com>
2022-10-29 22:21:11 +0700

    build: to do build explore more movie page 27/10

---
+++ pages/movie/explore/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="mx-16 mb-16 mt-32">
+    <div>Explore</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "index"
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
