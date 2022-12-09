7c10c03ff513659ecb7c090888a09d982a7e083c
thaihn28 <hoangthai.solem2801@gmail.com>
2022-12-10 06:23:09 +0700

    feat: fetch api similar movie, refactor code to watching movie

--- pages/movie/detail/_slug.vue
+++ pages/movie/detail/_slug.vue
@@ -1,25 +1,28 @@
 <template>
   <div class="mb-16">
-    <PosterDetail :movie="movie"/>
+    <PosterDetail :movie="movie" :similar-movies="similarMovies"/>
   </div>
 </template>
 
 <script>
 import PosterDetail from "@/components/Detail/PosterDetail";
 import {scrollToTop} from "@/utils/app_utils";
-import {mapGetters, mapMutations } from "vuex";
+import {mapGetters, mapMutations} from "vuex";
 
 export default {
+  middleware: 'authenticated',
   name: "MovieDetail",
   components: {
     PosterDetail
   },
-  async fetch ({ store, params }) {
+  async fetch({store, params}) {
     await store.dispatch('movie/fetchingMovieDetail', params.slug)
+    await store.commit('detail/setMovie', store.getters["movie/getMovieDetail"])
   },
-  data(){
-    return{
-      loading: false
+  data() {
+    return {
+      loading: false,
+      similarMovies: []
     }
   },
   computed: {
@@ -28,12 +31,23 @@ export default {
     }),
   },
   created() {
+    this.fetchingSimilarMovies()
     scrollToTop()
-    if(process.client){
-      const movieObj = JSON.stringify(this.movie)
-      localStorage.setItem('movie', movieObj)
-    }
   },
+  methods: {
+    async fetchingSimilarMovies() {
+      try {
+        const resource = 'api/v1/movies'
+        const slugs = this.$store.getters['movie/getMovieDetail'].categories.map((item) => (item.slug)).toString()
+        const res = await this.$axios.$get(`${resource}/filter-by-category?slug=` + slugs)
+        const similarMovies = res.content
+          .filter(item => (item.name !== this.movie.name))
+        this.similarMovies = similarMovies
+      } catch (e) {
+        console.error(e.response.data)
+      }
+    }
+  }
 }
 </script>
 
--- pages/movie/watching/_slug.vue
+++ pages/movie/watching/_slug.vue
@@ -3,11 +3,11 @@
     <div class="grid grid-cols-4 gap-10 container">
       <div class="col-span-3 col-start-1 flex flex-col  flex-grow text-white">
         <div class="relative flex w-full h-[560px] flex-shrink-0">
-          <iframe :src="movie?.episodes[0]?.linkEmbed || 'https://2embed.org/embed/movie?imdb=1'"
+          <iframe :src="linkEmbed"
                   class="relative w-full h-full top-0 left-0" frameborder="0" allowfullscreen=""></iframe>
         </div>
         <div class="flex flex-col flex-shrink-0 mt-5 gap-3">
-          <div class="text-white md:text-[30px] text-[35px] font-semibold ">{{movie.name}}</div>
+          <div class="text-white md:text-[30px] text-[35px] font-semibold ">{{ movie?.name }}</div>
           <div class="flex flex-row gap-4">
               <span class="flex flex-row gap-x-[4px]">
                 <icon-star class="mt-0.5"/>
@@ -15,28 +15,30 @@
               </span>
             <span class="flex flex-row gap-x-[4px]">
                 <icon-calendar class="mt-0.5 mr-0.5"/>
-                <span>{{movie.year}}</span>
+                <span>{{ movie?.year }}</span>
               </span>
           </div>
           <div class="flex-nowrap gap-2">
             <span class="md:text-xl text-2xl mb-2 font-semibold">Overview</span>
             <p class="text-gray-400 md:text-md text-xl">
-              {{movie.content}}
+              {{ movie?.content }}
             </p>
           </div>
         </div>
       </div>
       <div class="vertical-scroll text-white md:w-[352px] w-full flex-shrink-0 rounded-lg md:flex-col">
         <span class="text-2xl font-bold">Recommends</span>
-        <div class="w-full my-5 flex justify-between flex-row flex-shrink-0 gap-2 cursor-pointer" @click="viewOtherMovie()">
+        <div v-for="item in recommendMovies" :key="item.id" class="w-full my-5 flex justify-between flex-row flex-shrink-0 gap-2 cursor-pointer"
+             @click="viewOtherMovie(item.slug)">
           <div class="md:w-[100px] md:h-[150px] w-[170px] h-[250px] overflow-hidden rounded-lg flex-shrink-0">
-            <img src="https://image.tmdb.org/t/p/w500/uXEqmloGyP7UXAiphJUu2v2pcuE.jpg" alt="No image" class="w-full h-full object-contain object-center">
+            <img :src="item.thumbURL" alt="No image"
+                 class="w-full h-full object-contain object-center">
           </div>
           <div class="flex flex-col flex-grow gap-3 justify-center p-3 truncate">
-            <span class="md:text-xl text-2xl flex-grow-0 max-w-full truncate">Movie Name</span>
-            <span class="text-gray-400 mb-6 md:text-md text-lg">2006-07-06</span>
+            <span class="md:text-xl text-2xl flex-grow-0 max-w-full truncate">{{ item.name }}</span>
+            <span class="text-gray-400 mb-2 md:text-md text-lg">{{ item.year }}</span>
             <span class="flex flex-row items-center md:py-0 py-1 border rounded-2xl w-fit px-4 text-sm">
-                <span class="md:text-md text-xl">Actor</span>
+                <span class="md:text-base text-lg">{{ item.type }}</span>
               </span>
           </div>
         </div>
@@ -51,32 +53,45 @@ import iconCalendar from "assets/images/icons/iconCalendar";
 import {scrollToTop} from "@/utils/app_utils";
 
 export default {
+  validate({store}) {
+    const movie = store.getters['detail/getMovie']
+    return Object.keys(movie).length !== 0
+  },
+  middleware: 'authenticated',
   name: "WatchingMovie",
   components: {
     iconStar,
     iconCalendar,
   },
-  data(){
-    return{
+  data() {
+    return {
       loading: false,
+      movie: {},
+      linkEmbed: 'https://2embed.org/embed/movie?imdb=1'
     }
   },
   computed: {
-    movie(){
-      if(process.client)
-        console.log('===vao day')
-        return JSON.parse(localStorage.getItem('movie'))
+    recommendMovies(){
+      const movies = this.$store.getters['movie/getHotMovies']
+        .filter(item => (item.name !== this.movie.name))
+      return movies
     }
   },
   created() {
     scrollToTop()
-  },
-  destroyed() {
-    localStorage.removeItem('movie')
+    this.checkExistMovieData()
   },
   methods: {
-    viewOtherMovie(){
-      this.$router.push('/movie/watching/1')
+    checkExistMovieData(){
+      const stateMovie = this.$store.getters["detail/getMovie"]
+      if (stateMovie.slug === this.$route.params.slug) {
+        this.movie = stateMovie
+        this.linkEmbed = stateMovie?.episodes[0]?.linkEmbed
+      } else
+        this.$router.push('/error')
+    },
+    viewOtherMovie(slug) {
+      this.$router.push(`/movie/detail/${slug}`)
     }
   }
 }
