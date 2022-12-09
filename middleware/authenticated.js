302156b5031ef731a71d1d727c8c0e4192012ccb
thaihn28 <hoangthai.solem2801@gmail.com>
2022-12-10 06:23:37 +0700

    fix: refactor auth middleware

--- middleware/authenticated.js
+++ middleware/authenticated.js
@@ -1,7 +1,7 @@
 export default function (context) {
   // If the user is not authenticated
   const { redirect, store } = context
-  const isLoggedIn = context.$auth.$storage.getCookie('auth_loggedIn')
+  const isLoggedIn = store.state.user.user
   if (!store.state.auth.loggedIn && !isLoggedIn) {
     return redirect('/login')
   }
